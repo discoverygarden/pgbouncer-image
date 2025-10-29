13f354d5207a02b438fedf2405b276b0a29b8d66
Alexander Cairns <alexandercairns@discoverygarden.ca>
2025-10-29 10:43:35 -0300

    feat: support password auth

--- rootfs/opt/entry-point.sh
+++ rootfs/opt/entry-point.sh
@@ -3,10 +3,22 @@ set -eu
 
 EMPTY=""
 
+AUTH_TYPE=${AUTH_TYPE:-"md5"}
+
 if [ ! -f $USERLIST_PATH -a -n ${DRUPAL_DB_USER:-EMPTY} -a -n ${DRUPAL_DB_PASSWORD:-EMPTY} ] ; then
-  cat > $USERLIST_PATH <<EOF
-"$DRUPAL_DB_USER" "md5$(echo -n $DRUPAL_DB_PASSWORD$DRUPAL_DB_USER | md5sum - | cut -d" " -f1)"
-EOF
+  case $AUTH_TYPE in
+    md5)
+      credential="md5$(echo -n $DRUPAL_DB_PASSWORD$DRUPAL_DB_USER | md5sum - | cut -d" " -f1)"
+      ;;
+    password)
+      credential=$DRUPAL_DB_PASSWORD
+      ;;
+    *)
+      echo $AUTH_TYPE is not supported
+      exit 1
+      ;;
+  esac
+  echo "$DRUPAL_DB_USER $credential" > $USERLIST_PATH
 fi
 
 if [ -n ${PGHOST:-EMPTY} -a -n ${PGDATABASE:-EMPTY} -a -n ${PGUSER:-EMPTY} -a -n ${PGPASSWORD:-EMPTY} ] ; then
